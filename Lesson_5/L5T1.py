698af37385c3cd299b940f8613173ead3cbe2163
Irina Okuneva <okuneva_ira@vk.com>
2025-08-15 22:53:07 +0300

    Correction task 1 lesson 5

--- Lesson_5/L5T1.py
+++ Lesson_5/L5T1.py
@@ -3,11 +3,11 @@ import pandas as pd
 users = pd.read_csv('All_Files/users_new.csv')
 orders = pd.read_csv('All_Files/orders_new.csv')
 
-merged = orders.merge(users, on='user_id')
-
 north_ppl = users.query("region == 'North' & age < 30")
-north_orders = orders[orders['user_id'].isin(north_ppl['user_id'])]
 
-print(north_ppl)
-print(north_orders)
-print("Общее количество заказов:", len(north_orders))
+user_orders_count = orders.groupby('user_id')['order_id'].count().reset_index()
+user_orders_count.columns = ['user_id', 'order_count']
+
+result = north_ppl.merge(user_orders_count, on='user_id')[['name', 'order_count']]
+
+print(result)
